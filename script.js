d7e063e98cb267f2dadcf96543852bda76d127e1
Manish Kumar Barman <manishkumarbarman111@gmail.com>
2024-12-06 17:57:19 +0530

    add audio

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {});
+window.onload = function () {
+    var audio = document.getElementById("myAudio");
+    audio.play().catch(function (error) {
+        console.log("Autoplay was prevented. Error: ", error);
+    });
+};
